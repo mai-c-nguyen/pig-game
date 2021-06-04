005854fcd19f3206e26b0d7873f0c62801225d76
Mai Nguyen <mai.nguyen.gl@gmail.com>
2021-06-04 12:28:20 +0200

    JS update

--- bankist/script.js
+++ bankist/script.js
@@ -32,3 +32,16 @@ const account4 = {
   interestRate: 1,
   pin: 4444,
 };
+
+/////////////////////////////////////////////////
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
